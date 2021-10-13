bc0e9fc7ec20cd7e191eb1bc3fd026da35d9bb9e
ADobrynin <adobrynin@t1-consulting.ru>
2021-10-13 15:54:21 +0300

    Java code configurations

--- spring-java-config/src/main/kotlin/ru/sber/services/Services.kt
+++ spring-java-config/src/main/kotlin/ru/sber/services/Services.kt
@@ -1,14 +1,17 @@
 package ru.sber.services
 
 import org.springframework.stereotype.Component
+import org.springframework.stereotype.Repository
 import org.springframework.stereotype.Service
 
+@Repository
 class FirstService {
     override fun toString(): String {
         return "I am firstService"
     }
 }
 
+@Service
 class SecondService {
     override fun toString(): String {
         return "I am secondService"
