88e319764dab9592c49cb51596bf047202a92fae
ADobrynin <adobrynin@t1-consulting.ru>
2021-10-13 16:41:46 +0300

    optimized imports

--- spring-bean-scope/src/main/kotlin/ru/sber/services/Services.kt
+++ spring-bean-scope/src/main/kotlin/ru/sber/services/Services.kt
@@ -1,8 +1,6 @@
 package ru.sber.services
 
-import org.springframework.beans.factory.config.BeanDefinition
 import org.springframework.context.annotation.Scope
-import org.springframework.context.annotation.ScopedProxyMode
 import org.springframework.stereotype.Component
 
 @Component
