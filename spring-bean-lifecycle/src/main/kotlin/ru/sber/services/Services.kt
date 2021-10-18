f517dfcbe0063e91de8d8c4abd94e96b088c0305
ADobrynin <adobrynin@t1-consulting.ru>
2021-10-18 10:10:06 +0300

    lifecycle update

--- spring-bean-lifecycle/src/main/kotlin/ru/sber/services/Services.kt
+++ spring-bean-lifecycle/src/main/kotlin/ru/sber/services/Services.kt
@@ -19,7 +19,7 @@ class CallbackBean : InitializingBean, DisposableBean {
     }
 }
 
-class CombinedBean : BeanPostProcessor, InitializingBean {
+class CombinedBean : InitializingBean {
     var postProcessBeforeInitializationOrderMessage: String? = null
     var postConstructOrderMessage: String? = null
     var customInitOrderMessage: String? = null
@@ -37,16 +37,6 @@ class CombinedBean : BeanPostProcessor, InitializingBean {
     @PostConstruct
     fun postConstruct() {
         postConstructOrderMessage = "postConstruct() is called"
-    }
-
-    override fun postProcessBeforeInitialization(bean: Any, beanName: String): Any? {
-        postProcessBeforeInitializationOrderMessage = "postProcessBeforeInitialization() is called"
-        return super.postProcessBeforeInitialization(bean, beanName)
-    }
-
-    override fun postProcessAfterInitialization(bean: Any, beanName: String): Any? {
-        postProcessAfterInitializationOrderMessage = "postProcessAfterInitialization() is called"
-        return super.postProcessAfterInitialization(bean, beanName)
     }
 }
 
